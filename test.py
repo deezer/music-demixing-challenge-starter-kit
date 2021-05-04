337e2b6e3ddfd7e823b7efa9ee6ca2fc0f336b32
Félix Voituret <fvoituret@deezer.com>
2021-05-04 14:05:44 +0200

    🚀  finalize Spleeter predictor

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
-# This file is the entrypoint for your submission.
-# You can modify this file to include your code or directly call your functions/modules from here.
 from evaluator.music_demixing import MusicDemixingPredictor
 
-from spleeter import Separator
 from spleeter.audio.ffmpeg import FFMPEGProcessAudioAdapter
+from spleeter.separator import Separator
 
 
 class SpleeterPredictor(MusicDemixingPredictor):
@@ -13,10 +11,13 @@ class SpleeterPredictor(MusicDemixingPredictor):
     def prediction_setup(self) -> None:
         self.audio = FFMPEGProcessAudioAdapter()
         self.separator = Separator("spleeter:4stems")
-        # TODO: Force model downloading.
+        # NOTE: call manually internal to ensure model is
+        #       not lazy loaded.
+        self.separator._get_prediction_generator()
 
     def prediction(
         self,
+        music_name: str,
         mixture_file_path: str,
         bass_file_path: str,
         drums_file_path: str,
@@ -25,16 +26,15 @@ class SpleeterPredictor(MusicDemixingPredictor):
     ) -> None:
         y, _ = self.audio.load(mixture_file_path)
         prediction = self.separator.separate(y)
-        # TODO: map proper output name.
         instruments = {
             "bass": bass_file_path,
             "drums": drums_file_path,
-            "others": other_file_path,
+            "other": other_file_path,
             "vocals": vocals_file_path,
         }
         for instrument, path in instruments.items():
             self.audio.save(path, prediction[instrument], 44100)
-        print("%s: prediction completed." % mixture_file_path)
+        print("%s: prediction completed." % music_name)
 
 
 if __name__ == "__main__":
