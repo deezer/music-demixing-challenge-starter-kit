691a5b0d7d195969c44e833a4e83e86f6467a82b
Shivam Khandelwal <skbly7@gmail.com>
2021-05-12 19:42:13 +0530

    Fix for running spleeter baseline online

--- test.py
+++ test.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python
+#
+# This file uses spleeter for music demixing.
+#
+# NOTE: spleeter need checkpoints to be submitted along with your code.
+#
+# Making submission using openunmix:
+# 1. Run this file locally with `python test.py`.
+# 3. Submit your code using git-lfs
+#    #> git lfs install
+#    #> git lfs track "pretrained_models/*"
+#    #> git add .gitattributes
+#    #> mkdir -p pretrained_models/pretrained_models/
+#    #> ln -s ../4stems/ pretrained_models/pretrained_models/4stems
+#    #> git add pretrained_models/
+
+
 from evaluator.music_demixing import MusicDemixingPredictor
 
 from spleeter.audio.ffmpeg import FFMPEGProcessAudioAdapter
@@ -17,7 +33,6 @@ class SpleeterPredictor(MusicDemixingPredictor):
 
     def prediction(
         self,
-        music_name: str,
         mixture_file_path: str,
         bass_file_path: str,
         drums_file_path: str,
@@ -34,7 +49,7 @@ class SpleeterPredictor(MusicDemixingPredictor):
         }
         for instrument, path in instruments.items():
             self.audio.save(path, prediction[instrument], 44100)
-        print("%s: prediction completed." % music_name)
+        print("%s: prediction completed." % mixture_file_path)
 
 
 if __name__ == "__main__":
